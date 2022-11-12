04b67964bb82e47d35522a8de1e78eaf7e8a8147
Akif Mohammed <akiif.dev@gmail.com>
2022-11-12 19:05:08 +0530

    Updated the About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 const resumeLink = "https://drive.google.com/u/2/uc?id=14Cibaw_o5ERht5KviTBjWW_0Ezg1e6JR&export=download";
 
 function About() {
+    function calculate_age(dob) {
+        var diff_ms = Date.now() - dob.getTime();
+        var age_dt = new Date(diff_ms);
+
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
+    const myAge = calculate_age(new Date (2000, 1, 26));
+
     return (
         <section className='about colored-section' id='about'>
             <h2 className='title'>About</h2>
             <article>
-            I am <span className='bold'>Akif Mohammed</span>; An aspiring Software Engineer who loves all things tech. I love to explore new technologies.
-            I have completed my B.E.  in Information Science and Engineering from Mangalore, India.
+                I am <span className='bold'>Akif Mohammed</span>; A Software Engineer who loves all things tech. I love to explore new technologies.
+                <br /><br />
+                I have completed my bachelor's degree in "Information Science and Engineering" from Mangalore, India. I am {myAge} years old. My hobbies include 
+                watching movies, anime and reviews of new tech products, gaming and reading novels.
             </article>
 
             <a className='resume-btn btn' href={resumeLink} download={true}>Download Resume</a>
